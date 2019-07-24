31a62b4e18091fd076b6d9edd44d7579b664a487
machariamwangi <you@example.com>
2019-07-24 17:21:45 -0400

    first commit and final

---
+++ resources/views/admin/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.admin')
+
+@section('content')
+@if (count($errors->all()))
+    
+<div class="row">
+    <div class="col-md-12">
+        <div class="alert alert-danger">
+            <ul>
+
+            </ul>
+        </div>
+    </div>
+</div>
+@endif
+    <div class="row">
+        <div class="col-md-12">
+            <form action="{{ route('admin.create') }}" method="post">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" id="title" name="title">
+                </div>
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <input type="text" class="form-control" id="content" name="content">
+                </div>
+                {{ csrf_field() }}
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-default">
+<div class="container-fluid">
+    <div class="navbar-header">
+        <a href="#" class="navbar-brand">laravel guide</a>
+        <ul class="nav navbar-nav">
+        <li><a href="{{route('blog.index')}}">Blog</a></li>
+        <li><a href="{{route('other.about')}}">About</a></li>
+        </ul>
+    </div>
+</div>
+</nav>
